3d21c207bfd6203c39d38898bdcffaf35ba39e0f
Maknollram <maknollram@hotmail.com>
2022-04-22 18:35:43 -0300

    first commit

---
+++ Assets/Scripts/Aviao.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aviao : MonoBehaviour {
+    private Rigidbody2D fisica;
+    [SerializeField]
+    private float forca;
+    private Diretor diretor;
+    private Vector3 posicaoInicial;
+    private bool deveImpulsionar;
+    private Animator animacao;
+
+    private int counter = 240;
+
+    private void Awake()
+    {
+        this.posicaoInicial = this.transform.position;
+        this.fisica = this.GetComponent<Rigidbody2D>();
+        this.animacao = this.GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        this.diretor = GameObject.FindObjectOfType<Diretor>();
+        this.fisica.simulated = false;
+    }
+
+    private void Update () {
+        counter--;
+        if (counter == 0 || Input.GetButtonDown("Fire1") || Input.GetKeyDown(KeyCode.Alpha0) || Input.GetKeyDown(KeyCode.Keypad0))
+            this.fisica.simulated = true;
+        if (Input.GetButtonDown("Fire1") || Input.GetKeyDown(KeyCode.Alpha0) || Input.GetKeyDown(KeyCode.Keypad0))
+        {
+            this.deveImpulsionar = true;
+        }
+        this.animacao.SetFloat("VelocidadeY", this.fisica.velocity.y);
+	}
+
+    private void FixedUpdate()
+    {
+        if(this.deveImpulsionar)
+        {
+            this.Impulsionar();
+        }
+        
+    }
+
+    public void Reiniciar()
+    {
+        this.transform.position = this.posicaoInicial;
+        this.fisica.simulated = true;
+    }
+
+    private void Impulsionar()
+    {
+        this.fisica.velocity = Vector2.zero;
+        this.fisica.AddForce(Vector2.up * this.forca, ForceMode2D.Impulse);
+        this.deveImpulsionar = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D colisao)
+    {
+        this.fisica.simulated = false;
+        this.diretor.FinalizarJogo();
+    }
+}
